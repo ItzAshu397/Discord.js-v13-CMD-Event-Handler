680a5db509dcb7d65010850089d65f6887fce339
Ashutosh Swamy <76511506+ItzAshu397@users.noreply.github.com>
2021-10-22 14:04:02 +0530

    Add files via upload

---
+++ src/events/ready.js
@@ -0,0 +1,9 @@
+const chalk = require("chalk");
+
+module.exports = (client) => {
+  console.log(
+    `${chalk.bold.green("[BOT]")} Logged in as ${client.user.username}#${
+      client.user.discriminator
+    }.`
+  );
+};
